fef8a8629ca9c1710a9a7a019e7c0dad6a56f75f
Auke Booij <auke@tulcod.com>
2018-05-11 14:01:30 +0100

    Reduce assumptions on the rationals to avoid universe size issues

--- theories/Classes/theory/rationals.v
+++ theories/Classes/theory/rationals.v
@@ -25,7 +25,8 @@ Context {Q : Type@{UQ} } {Qap : Apart@{UQ UQ} Q}
   {Qzero : Zero Q} {Qone : One Q} {Qneg : Negate Q} {Qrecip : DecRecip Q}
   {Qle : Le@{UQ UQ} Q} {Qlt : Lt@{UQ UQ} Q}
   {QtoField : RationalsToField@{UQ UQ UQ UQ} Q}
-  {Qrats : Rationals@{UQ UQ UQ UQ UQ UQ UQ UQ UQ UQ} Q}
+  {Qfield : DecField@{UQ} Q}
+  {Qfpsro : FullPseudoSemiRingOrder@{UQ UQ UQ UQ UQ UQ UQ} Qle Qlt}
   {Qtrivialapart : TrivialApart Q} {Qdec : DecidablePaths Q}
   {Qmeet : Meet Q} {Qjoin : Join Q} {Qlattice : LatticeOrder Qle}
   {Qle_total : TotalRelation (@le Q _)}
